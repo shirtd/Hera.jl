989b774096e5437c2c21743304a8ea3afe0eb635
kirk gardner <kirk@worm.local>
2018-05-28 20:43:52 -0400

    deps

--- src/Hera.jl
+++ src/Hera.jl
@@ -3,8 +3,10 @@ module Hera
 
     export bottleneck
 
-    bottleneck(A,B) = ccall((:bottleneck, "deps/usr/lib/libhera"),
+    function bottleneck(A,B)
+        ccall((:bottleneck, "deps/usr/lib/libhera"),
             Float64,(Cint, Cint, Ptr{Float64}, Ptr{Float64}),
             size(A,1), size(B,1), A'[:], B'[:])
+    end
 
 end
