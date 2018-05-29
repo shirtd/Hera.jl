f6434f344fc8ec56b5c047c8c737536cbf8dd275
kirk gardner <kirk@worm.local>
2018-05-28 21:31:00 -0400

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,7 @@ if !("hera" in readdir("src"))
     run(`git clone https://bitbucket.org/grey_narn/hera.git src/hera`)
 end
 
+mkpath("usr/lib")
 INC = "-Isrc/hera/geom_bottleneck/include"
 provides(BuildProcess, (@build_steps begin
     @build_steps begin
