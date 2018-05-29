568882678d2bc3b54ef63a08079d9b3ba9333bfb
kirk gardner <kirk@worm.local>
2018-05-28 21:45:08 -0400

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -4,23 +4,20 @@ using BinDeps
 
 libhera = library_dependency("libhera")
 
-if !("hera" in readdir("src"))
-    run(`git clone https://bitbucket.org/grey_narn/hera.git src/hera`)
-end
-
-mkpath("usr/lib")
-INC = "-Isrc/hera/geom_bottleneck/include"
+URL = "https://bitbucket.org/grey_narn/hera.git"
+SRC = "src/hera"
+LIB = "usr/lib"
+SUFF = is_apple() ? "dylib" : "so"
+INC = "-I$SRC/geom_bottleneck/include"
 provides(BuildProcess, (@build_steps begin
+    !("hera" in readdir("src")) ? run(`git clone $URL $SRC`) : println("$SRC exists")
     @build_steps begin
-        FileRule(joinpath("usr","lib","libhera.dylib"), @build_steps begin
-            `g++ -c src/hera.cpp $INC -o usr/lib/hera.o`
-            `g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`
+        FileRule(joinpath("usr","lib","libhera.$suff"), @build_steps begin
+            `mkdir $LIB`
+            `g++ -c src/hera.cpp $INC -o $LIB/hera.o`
+            `g++ -static-libstdc++ -dynamiclib -fPIC $INC -o $LIB/libhera.$suff $LIB/hera.o`
         end)
     end
 end), libhera)
-
-# mkpath("usr/lib")
-# run(`g++ -c src/hera.cpp $INC -o usr/lib/hera.o`)
-# run(`g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`)
 
 @BinDeps.install Dict(:libhera => :libhera)
