cc94e8f22c4ca68f26bb5504c4e2ae0c76f6fa55
kirk gardner <kirk@worm.local>
2018-05-28 21:55:15 -0400

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -11,9 +11,9 @@ SUFF = is_apple() ? "dylib" : "so"
 INC = "-I$SRC/geom_bottleneck/include"
 provides(BuildProcess, (@build_steps begin
     !("hera" in readdir("src")) ? run(`git clone $URL $SRC`) : println("$SRC exists")
+    mkdir(LIB)
     @build_steps begin FileRule(joinpath("usr","lib","libhera.$SUFF"),
         @build_steps begin
-            `mkdir $LIB`
             `g++ -c src/hera.cpp $INC -o $LIB/hera.o`
             `g++ -static-libstdc++ -dynamiclib -fPIC $INC -o $LIB/libhera.$SUFF $LIB/hera.o`
         end)
