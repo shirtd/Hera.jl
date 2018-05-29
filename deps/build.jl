40cb3f03b90fa6e413f41b8ad303644f04858e40
kirk gardner <kirk@worm.local>
2018-05-28 20:56:59 -0400

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -10,5 +10,3 @@ mkpath("usr/lib")
 INC = "-Isrc/hera/geom_bottleneck/include"
 run(`g++ -c src/hera.cpp $INC -o usr/lib/hera.o`)
 run(`g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`)
-
-libhera = library_dependency("libhera")
--- src/Hera.jl
+++ src/Hera.jl
@@ -1,12 +1,15 @@
 __precompile__(true)
+
 module Hera
 
-    export bottleneck
+    libhera = joinpath(dirname(@__FILE__),"..","deps","usr","lib","libhera")
 
     function bottleneck(A,B)
         ccall((:bottleneck, libhera),
             Float64,(Cint, Cint, Ptr{Float64}, Ptr{Float64}),
             size(A,1), size(B,1), A'[:], B'[:])
     end
+
+    export bottleneck
 
 end
