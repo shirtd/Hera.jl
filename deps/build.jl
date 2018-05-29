7983078c2b0c4f0b988396490cf653f5bf9e5435
kirk gardner <kirk@worm.local>
2018-05-28 21:26:37 -0400

    deps

--- deps/build.jl
+++ deps/build.jl
@@ -2,15 +2,24 @@ using BinDeps
 
 @BinDeps.setup
 
+libhera = library_dependency("libhera")
+
 if !("hera" in readdir("src"))
     run(`git clone https://bitbucket.org/grey_narn/hera.git src/hera`)
 end
 
-mkpath("usr/lib")
 INC = "-Isrc/hera/geom_bottleneck/include"
-run(`g++ -c src/hera.cpp $INC -o usr/lib/hera.o`)
-run(`g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`)
+provides(BuildProcess, (@build_steps begin
+    @build_steps begin
+        FileRule(joinpath("usr","lib","libhera.dylib"), @build_steps begin
+            `g++ -c src/hera.cpp $INC -o usr/lib/hera.o`
+            `g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`
+        end)
+    end
+end), libhera)
 
-libhera = library_dependency("libhera")
+# mkpath("usr/lib")
+# run(`g++ -c src/hera.cpp $INC -o usr/lib/hera.o`)
+# run(`g++ -static-libstdc++ -dynamiclib -fPIC $INC -o usr/lib/libhera.dylib usr/lib/hera.o`)
 
 @BinDeps.install Dict(:libhera => :libhera)
